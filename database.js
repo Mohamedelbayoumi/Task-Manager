47942df5f6c8e8e62ebd643b4477a81c9d9b20c2
Mohamedelbayoumi <mohamedalbyoumi822@gmail.com>
2024-07-28 03:25:16 +0300

    final commit

--- database.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize} = require('sequelize')
-const sequelize = new Sequelize('task-manager','root','mentafi',
-{
-    dialect:'mysql',
-    host : 'localhost',
-    port : 3000,
-})
-module.exports = sequelize
